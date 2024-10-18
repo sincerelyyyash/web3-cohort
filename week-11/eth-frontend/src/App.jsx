b3b36d3f0fc53042cc3faa3df431ef8944159139
sincerelyyyash <yta0088@gmail.com>
2024-10-18 21:45:09 +0530

    Connected to wallet and init txn

--- week-11/eth-frontend/src/App.jsx
+++ week-11/eth-frontend/src/App.jsx
@@ -1,21 +1,72 @@
-import { createPublicClient, http } from 'viem'
-import { mainnet } from 'viem/chains'
 
-const client = createPublicClient({
-  chain: mainnet,
-  transport: http(),
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { sendTransaction } from 'viem/actions'
+import { http, createConfig, WagmiProvider, useBalance, useAccount, useConnect } from 'wagmi'
+import { mainnet } from 'wagmi/chains'
+import { injected } from 'wagmi/connectors'
+
+export const config = createConfig({
+  chains: [mainnet],
+  connectors: [
+    injected(),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+  },
 })
 
+const queryClient = new QueryClient()
+
 function App() {
-  async function getBalance() {
-    const balance = await client.getBalance({ address: "0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD" })
-    console.log(balance);
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <WalletConnector />
+          <EthSend />
+          <MyAddress />
+        </div>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+function MyAddress() {
+  const { address } = useAccount()
+  const balance = useBalance({ address })
+
+  return <div>
+    {address}
+    {balance?.data?.formatted}
+    console.log(balance)
+  </div>
+}
+
+function WalletConnector() {
+  const { connectors, connect } = useConnect()
+
+  return connectors.map((connector) => (
+    <button key={connector.uid} onClick={() => connect({ connector })}>
+      {connector.name}
+    </button>
+  ))
+}
+
+function EthSend() {
+  function sendEth() {
+    const address = document.getElementById("address").value;
+
+    sendTransaction({
+      to: address,
+      value: "100000000000000000"
+    })
   }
 
   return (
     <div>
-      <button onClick={getBalance}>Get Balance </button></div>
-
+      <input id="address" type="text" placeholder='Address...'></input>
+      <button onClick={sendEth}>Send ETH</button>
+    </div>
   )
 }
 
